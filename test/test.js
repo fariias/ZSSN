05a5e77f940d6b5f31cc79da9718dbb5620953fd
junior <josenildoaf@gmail.com>
2017-05-15 09:08:46 -0300

    adding the report infected test

--- test/test.js
+++ test/test.js
@@ -8,6 +8,8 @@ var config = require('../config');
 
 describe('Routing', function () {
     var url = '127.0.0.1:8080';
+    var id1 = ''; // for first user test
+    var id2 = '';// for second user test
 
     before(function (done) {
         mongoose.connect(config.db.mongodb);
@@ -40,9 +42,19 @@ describe('Routing', function () {
             "infected": false,
             "reports": 0
         }
-        
 
-        it('checking the create an survivor', function (done) {
+        var body2 = {
+            "name": "Peterson",
+            "gender": "M",
+            "age": 37,
+            "location": { "latitude": "-8.9999", "longitude": "-10.87584" },
+            "inventory": { "water": 5, "ammunation": 4, "food": 0, "medication": 5 },
+            "infected": false,
+            "reports": 0
+        }
+
+
+        it('checking the create an first survivor', function (done) {
             request(url)
                 .post('/api/survivors')
                 .send(body)
@@ -54,18 +66,57 @@ describe('Routing', function () {
                     }
                     res.body.should.have.property('_id');
                     res.body.name.should.equal('Junior');
+                    id = res.body._id;
                     done();
                 });
         });
 
-        it('checking the update survival location', function (done) {
+        it('checking the create an second survivor', function (done) {
+            request(url)
+                .post('/api/survivors')
+                .send(body2)
+                .expect('Content-Type', /json/)
+                .expect(200)
+                .end(function (err, res) {
+                    if (err) {
+                        throw err;
+                    }
+                    res.body.should.have.property('_id');
+                    res.body.name.should.equal('Peterson');
+                    id = res.body._id;
+                    done();
+                });
+        });
+
+        it('checking update surviver location', function (done) {
 
             var body = {
-               "location" : { "latitude": "-3.9999", "longitude": "-5.3333" }
+                "location": { "latitude": "-3.9999", "longitude": "-5.3333" }
             }
 
             request(url)
-                .put('/api/survivors/5918f6776acab122d6c5c4e5')
+                .put('/api/survivors/' + id)
+                .send(body)
+                .expect('Content-Type', /json/)
+                .expect(200) //Status code
+                .end(function (err, res) {
+                    if (err) {
+                        throw err;
+                    }
+                    res.body.location.latitude.should.equal(body.location.latitude);
+                    res.body.location.longitude.should.equal(body.location.longitude);
+                    done();
+                });
+        });
+
+        it('checking the report infected survival', function (done) {
+            var body = {
+                "report_id": id2,
+                "survivor_id": id1
+            }
+
+            request(url)
+                .put('/api/survivors/' + id +'/report')
                 .send(body)
                 .expect('Content-Type', /json/)
                 .expect(200) //Status code
